9f50b91459d2365c8106b2cca89000c09e218353
longlinh123456 <nope@example.com>
2022-01-02 17:21:21 +0700

    fixed keep alive log message again

--- keepAlive.ts
+++ keepAlive.ts
@@ -1,7 +1,7 @@
 import express from "express"
 const app = express()
 const listener = app.listen(process.env.PORT, function() {
-	console.log("Your app is listening on port " + (process.env.PORT ? process.env.PORT : "(failed to detect port)"))
+	console.log(`Your app is listening on port ${process.env.PORT}`)
 })
 app.get("/", (req, res) => res.sendStatus(200))
 process.on("SIGINT", () => {
