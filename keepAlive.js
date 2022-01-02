336f73527c41c7a223658ad1c9e6273316fae8d4
longlinh123456 <nope@example.com>
2022-01-02 12:45:54 +0700

    respond with http 200 for keepalive

--- keepAlive.js
+++ keepAlive.js
@@ -8,6 +8,7 @@ const app = express_1.default();
 const listener = app.listen(process.env.PORT, function () {
     console.log("Your app is listening on port " + process.env.port ? process.env.port : "(failed to detect port)");
 });
+app.get("/", (req, res) => res.sendStatus(200));
 process.on("SIGINT", () => {
     listener.close();
 });
--- keepAlive.ts
+++ keepAlive.ts
@@ -3,6 +3,7 @@ const app = express()
 const listener = app.listen(process.env.PORT, function() {
 	console.log("Your app is listening on port " + process.env.port ? process.env.port : "(failed to detect port)")
 })
+app.get("/", (req, res) => res.sendStatus(200))
 process.on("SIGINT", () => {
 	listener.close()
 })
